afa7161765d4c151556752233eef8cfc080df611
mggbhn <maggiebswim@gmail.com>
2018-07-17 17:46:21 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,14 +1,15 @@
 class Dog 
-  attr_accessor :name,:breed,:age 
+  attr_accessor :name,:breed,:age, :array 
   
   def initialize(name,breed,age)
     @name = name 
     @breed = breed
     @age = age 
+    @array = array
   end 
   
   def all
-    
+    array = []
   end 
   
 end
